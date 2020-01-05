b03216a69062a55cdf087813bbb3b918a86aab1e
symphograph <58216294+symphograph@users.noreply.github.com>
2020-01-06 07:05:30 +1100

    del trash

--- js/hides.js
+++
@@ -1,9 +0,0 @@
-function showInnerList(ilndex) {
-	 var elNavbar = Ext.get("navbar");
-	 var ceInnerLists = elNavbar.select("UL");
-	 ceInnerLists.setDisplayed(false);
-	 if (ilndex) {
-var sSelector = "UL:nth(" + iIndex + ")";
-elNavbar.child(sSelector).setDisplayed(true);
-}
-}
