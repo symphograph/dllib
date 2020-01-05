1077a07e976a0061fa6828b360219b241811b0fd
symphograph <58216294+symphograph@users.noreply.github.com>
2020-01-06 06:51:37 +1100

    missed_file

---
+++ usercheck.php
@@ -0,0 +1,10 @@
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'].'/includs/ip.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/includs/config.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/functions/functions.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/functions/functs.php';
+
+$userinfo_arr = Metka($dbLink,$ip);
+$mailuserid = $userinfo_arr['muser'];
+//var_dump($identy);
+?>
