7e5622719c30a56357c724ee8de137858762cf32
symphograph <58216294+symphograph@users.noreply.github.com>
2020-01-10 05:32:33 +1100

    recurs clean

--- includs/recurs.php
+++ includs/recurs.php
@@ -46,8 +46,6 @@ $crftorder[] = $item_id;
 //Если есть непосчитанные рецепты, считаем
 if($b > 0)
 {
-	
-	//orrepcost($user_id, $server_group);
 	if(!isset($orcost))
 	$orcost = PriceMode(2,$user_id)['auc_price'];
 	
@@ -57,121 +55,7 @@ if($b > 0)
 	//printr($craftkeys1);
 	
 	$craftarr = CraftsBuffering($craftkeys1);
-	///Запускаем расчет крафта выбранных предметов.
-	//var_dump($craftkeys1);
-/*
-	foreach($craftkeys1 as $key => $v)
-	{	
-		$krytery2 = array();
-		//echo '<br>'.$key.' имеет '.count($v).' рецептов: '.implode(",", $v).'<br>';
-		//exit();
-		$user_choose = 0; $best_type = 0;
-		//Если рецептов несколько
-		if (count($v)>1)
-		{$r = 0;
-		 
-		 	
-			 for ($i = 1; $i <= count($v); $i++)
-			 {
-
-					$rv = $v[$r]; 	
-					if(array_key_exists($rv,$userbest) and $best_type < 2)
-				   {$best_type = $userbest[$rv];
-				   $user_choose = $rv;
-				   }
-				
-
-					$mycost = rescost($rv, $forlost)[0]; $r++;
-					//var_dump($item_id);
-					
-					//echo $itog.' за '.$total.'<br>';
-					//$mycost= round($total/$itog,0);
-				 
-				 	//var_dump($mycost);
-					$krytery = $mycost;
-					if ($mycost != 0)
-					{
-						$krytery2[intval($craft_id)] = $mycost;
-						$mycost= round($mycost,0);
-						$mycost2[$craft_id] = $mycost;
-					};
-
-					$itemarr[] = $item_id;
-					$craftarr[] = $craft_id;
-					$mycostarr[] = $mycost;
-				 	//$or_arr[]= $total_or;
-					$bestcraft[] = 0;
-					$auccraft[] = 0;
-			 }
-			if($best_type < 2)
-			{
-				
-					$min = min($krytery2);
-				//echo $min;
-					$recomend_craft = array_search($min,$krytery2);
-				//echo '$best_type < 2 '.$recomend_craft;
-				//var_dump($krytery2);
-			}
-			if($best_type > 1) $recomend_craft = $user_choose;
-			$recomend_mycost = $mycost2[$recomend_craft];
-			//var_dump($recomend_mycost);
-
-			//echo '<p>Рекомендуемый рецепт: '.$recomend_craft.'</p>';
-			//echo '<p>Стоимость предмета по этому рецепту: '.$recomend_mycost.'</p>';
-		
-			$itemarr[] = $item_id;
-			$craftarr[] = $recomend_craft;
-			$mycostarr[] = $recomend_mycost;
-		 	//$or_arr[]= $total_or;
-			if($best_type < 2)
-			$bestcraft[] = 1; else $bestcraft[] = $best_type;
-			if($best_type < 3)
-			$forlost[$item_id] = $recomend_mycost;
-			if($best_type == 3)
-			$forlost[$item_id] = $userbestaucprice[$recomend_craft];
-			if($best_type == 3)
-			$auccraft[] = $userbestaucprice[$recomend_craft];
-			else $auccraft[] = 0;
-		//echo '<p>'.$item_id.' '.$recomend_mycost.'</p>';
-		}
-		else
-		//Если рецепт только один
-		{ 
-			if (count($v)>0)
-			{ 
-				$user_choose = 0; $best_type = 0;
-				$r = 0;
-				$rv = $v[$r];
-				
-				if(array_key_exists($rv,$userbest) and $best_type < 2)
-				{
-					$best_type = $userbest[$rv];
-					$user_choose = $rv;
-				}
-				
-				$mycost = rescost($rv, $forlost)[0];
-				//var_dump($total);
-				//$mycost= round($total/$itog,0);
-				
-
-				$itemarr[] = $item_id;
-				$craftarr[] = $craft_id;
-				$mycostarr[] = $mycost;
-				//$or_arr[]= $total_or;
-				if($best_type < 2)
-				$bestcraft[] = 1; else $bestcraft[] = $best_type;
-				if($best_type < 3)
-				  $forlost[$item_id] = $mycost;
-				 if($best_type == 3)
-				$forlost[$item_id] = $userbestaucprice[$craft_id];
-				if($best_type == 3)
-				 $auccraft[] = $userbestaucprice[$craft_id];
-				 else $auccraft[] = 0;
-				//echo '<p>'.$item_id.' '.$mycost.'</p>';
-		   }
-		 };
-	}; 
-*/		
+	
 		if(!in_array($_SERVER['SCRIPT_NAME'],['/hendlers/packs_list.php']))
 		{
 			if(count($lost)>0)
@@ -181,51 +65,8 @@ if($b > 0)
 			}
 		}
 		
-
 	$i = 0;
-/*
-	for ($i=0; $i < count($craftarr); $i++)
-	{
-		
-			$mycst = $mycostarr[$i];
-		//echo '$craftarr: '.$craftarr[$i].' | '.$mycst.'<br>';
-			
-		if($itemarr[$i] > 0 and count($lost)==0)
-		{
-			///Пишем в базу чего посчитали.
-			//echo '<p>'.$i.'</p>';
-			$sql="REPLACE INTO `user_crafts` (
-			`item_id` , `user_id`, `isbest`, `craft_id`, `craft_price`, `auc_price` ,`updated`) 
-			VALUES ('".$itemarr[$i]."', 
-			'".$user_id."', 
-			'".$bestcraft[$i]."', 
-			'".$craftarr[$i]."', 
-			'$mycst', 
-			'".$auccraft[$i]."' 
-			,now())";
-
-			//qwe($sql);
-			//echo 'cr: '.$craftarr[$i].' | it: '.$itemarr[$i].' | b: '.$bestcraft[$i].'<br>';
-			
-			if(in_array($bestcraft[$i],[1,2]))
-			{
-				$best_itarr[] = $craftarr[$i];
-				//echo $best_itarr[$i].'<br>';
-			}
-		 }
-		
-	}
-*/
 }
 
-
-//printr($craftarr);
-//$craftarr = [9257];
-//echo $_SERVER['SCRIPT_NAME'];
-//$NeedOrScripts = ['/catalog.php','/hendlers/item.php'];
-//if(in_array($_SERVER['SCRIPT_NAME'],$NeedOrScripts))//Фул ОР считаем только для крафкулятора.
-	AllOrRecurs($craftarr,$user_id);
-
-//if(in_array($_SERVER['SCRIPT_NAME'],$NeedOrScripts))
-
+AllOrRecurs($craftarr,$user_id);
 ?>
