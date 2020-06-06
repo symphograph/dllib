67beaecaebe1d4d2e9498af3b73615ae1d0b42a4
symphograph <58216294+symphograph@users.noreply.github.com>
2020-06-06 18:23:36 +1100

    add cleaner

---
+++ cleaner.php
@@ -0,0 +1,7 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'].'/includs/ip.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/includs/config.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/functions/functions.php';
+
+dbCleaner();
+?>
