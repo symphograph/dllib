7b371d79ec49f1f1a96f6569b5dc214d8425dfa1
symphograph <58216294+symphograph@users.noreply.github.com>
2023-01-25 01:07:13 +1100

    del trash

--- public_html/api/categories.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once dirname($_SERVER['DOCUMENT_ROOT']) . '/includs/config.php';
-
-use api\{Api, Category};
-$List = Category::getTree();
-echo Api::resultData($List);
--- public_html/api/search.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once dirname($_SERVER['DOCUMENT_ROOT']).'/includs/config.php';
-
-use api\{Api, item\Item};
-
-$List = Item::searchList();
-echo Api::resultData($List);
